2122e0c60c05ce02e5f297201aee9a4b7c3f6244
bhach <bhaskaradari8@gmail.com>
2021-03-16 17:42:42 +0000

    Added step by step process to setup GitHub to Rstudio

---
+++ GitHub Setup in RStudio Cloud.R
@@ -0,0 +1,14 @@
+#How to setup GitHub to RStudio Cloud
+#Step1: Create an account on GitHub
+#Step2: Create an empty repository in Github
+#Step3: Create a new project in RStudio Cloud - RStudio Cloud > “New Project" > “New Project from Git Repository"
+#Step4: Click on the Terminal tab and add "git config --global user.email "youremail@email.com"
+#Step5: Click on the Terminal tab and add "git config --global user.name "yourusername"
+#Step6: Click on the Terminal tab and add "git config remote.origin.url git@github.com:yourusername/Reponame.git"
+#Step7: Go to Tools > Global Options > Git/SVN > Create Public Key and then dont fill anything and click create.
+#Step8: Once public key is created, Click "View Public Key" and Copy it
+#Step9: Go to GitHub > Settings > SSH Keys > Add Key and Paste the code
+
+Now enjoy connected to Git all the time and do push and pull
+
+
