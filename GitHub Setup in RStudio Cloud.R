89efc7827e758e5dea38b55451b4b4e62118f7a2
bhach <bhaskaradari8@gmail.com>
2021-03-16 21:01:22 +0000

    Add

--- GitHub Setup in RStudio Cloud.R
+++ GitHub Setup in RStudio Cloud.R
@@ -11,9 +11,31 @@
 
 Now enjoy connected to Git all the time and do push and pull
 
-Clone a existing repo
+How to Import Complete repository/Clone a existing repo
 #Create a directory
 #Enter in Terminal "git clone clone_url"
 
 
+How to Import a data file from github
+#Go to the file and click raw
+#Copy the url
+#Install and load downloader package
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
 
+
+How to Install packages from GitHub and read files from github
+install.packages("devtools")
+library(devtools)
+install_github("genomicsclass/dagdata")
+dir <- system.file(package="dagdata") #extracts the location of package
+list.files(dir)
+list.files(file.path(dir,"extdata")) #external data is in this directory
+
+And now we are ready to read in the file:
+  
+filename <- file.path(dir,"extdata/femaleMiceWeights.csv")
+dat <- read.csv(filename)
+dat
