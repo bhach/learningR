f0eff682c480cc2c85ce080f0746ba82957abe0f
bhach <bhaskaradari8@gmail.com>
2021-03-20 15:56:37 +0000

    Changes

---
+++ Mini Projects to Practise/Airquality dataset.R
@@ -0,0 +1,24 @@
+#Airquality dataset:
+  
+Install.packages(datasets)
+library(datasets)
+airquality
+
+#Understand the terms of datasets
+?airquality
+
+#Observe the head of dataset
+head(dataset)
+
+#Questions
+#1. What are the dimensions of dataset?
+#2. What type of data is there in the dataset? (Categorical/Quantitative)
+#3. Any Null Values?
+#4. What are the column names?
+#5. Observe the class, str and typeof?
+#6. Select 10 random observations
+#7. Add an new colums for temp in celsus
+#8. Subset the data where Month is more than 5 and Temp is more then 70?
+#9. Mean of temperature for the complete data
+#10. 
+  
